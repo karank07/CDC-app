98f397711d67f53587d25795cddfb593d51edd25
Pranal PradeepKumar Kuttappan <pranalpk8@gmail.com>
2021-04-04 22:09:02 -0400

    Added report API admin control

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,55 +2,64 @@ import asyncHandler from "express-async-handler";
 import Patient from "../Models/patientModel.js";
 import Nurse from "../Models/nurseModel.js";
 import Doctor from "../Models/doctorModel.js";
+import moment from "moment";
 
 //@desc Get list of patients
 //@route GET /api/admins/patient-list
 //@access Protected admin
 const getPatientList = asyncHandler(async (req, res) => {
-    const keyword = req.query.keyword
+  const keyword = req.query.keyword
     ? {
         name: {
           $regex: req.query.keyword,
-          $options: 'i',
+          $options: "i",
         },
       }
-    : {}
-    const patientList = await Patient.find({...keyword}).select("-password")
-    res.json(patientList)
-})
+    : {};
+  const patientList = await Patient.find({ ...keyword }).select("-password");
+  res.json(patientList);
+});
 
 //@desc Get list of nurses
 //@route GET /api/admins/nurse-list
 //@access Protected admin
 const getNurseList = asyncHandler(async (req, res) => {
-    const nurseList = await Nurse.find().select("-password")
-    res.json(nurseList)
-})
+  const nurseList = await Nurse.find().select("-password");
+  res.json(nurseList);
+});
 
 //@desc Get list of doctors
 //@route GET /api/admins/doctor-list
 //@access Protected admin
 const getDoctorList = asyncHandler(async (req, res) => {
-    const doctorList = await Doctor.find().select("-password")
-    res.json(doctorList)
-})
+  const doctorList = await Doctor.find().select("-password");
+  res.json(doctorList);
+});
 
 //@desc delete a user
 //@route DELETE /api/admins/delete-user/:id
 //@access Protected admin
 const deleteUser = asyncHandler(async (req, res) => {
-  const user = (await Patient.findById(req.params.id)) ||
-  (await Nurse.findById(req.params.id)) ||
-  (await Doctor.findById(req.params.id))
-  if(user){
-    await user.remove()
-    res.json({success:true})
-  }
-  else{
+  const user =
+    (await Patient.findById(req.params.id)) ||
+    (await Nurse.findById(req.params.id)) ||
+    (await Doctor.findById(req.params.id));
+  if (user) {
+    await user.remove();
+    res.json({ success: true });
+  } else {
     res.status(404);
-    throw new Error('User not found')
+    throw new Error("User not found");
   }
-})
+});
 
-
-export {getPatientList, getNurseList, getDoctorList,deleteUser}
+//@desc Get daily, weekly, monthly patient report
+//@route GET /api/admins/report
+//@access Protected admin
+const getReport = asyncHandler(async (req, res) => {
+  const {from, to} = req.body
+  const patientList = await Patient.countDocuments({"assessments.createdAt":{$gte: from, $lte: to}});
+  res.json(patientList);
+});
+// moment(Date.now()).format("YYYY-MM-DD")
+export { getPatientList, getNurseList, getDoctorList, deleteUser, getReport };
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { deleteUser, getDoctorList, getNurseList, getPatientList } from '../controllers/adminController.js'
+import { deleteUser, getDoctorList, getNurseList, getPatientList, getReport } from '../controllers/adminController.js'
 import { protect,admin } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 router.route('/patient-list').get(protect,admin,getPatientList)
+router.route('/report').get(protect,admin,getReport)
 router.route('/nurse-list').get(protect,admin,getNurseList)
 router.route('/doctor-list').get(protect,admin,getDoctorList)
 router.route('/delete-user/:id').delete(protect,admin,deleteUser)
